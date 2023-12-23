eee234425906c1de28eaaa2331ffe70f6dd66d8e
it4545454545 <104487863+it4545454545@users.noreply.github.com>
2023-12-23 20:49:47 +0400

    Update ThreadLocalExample.java

--- ThreadLocalExample.java
+++ ThreadLocalExample.java
@@ -12,6 +12,8 @@ public class ThreadLocalExample {
             userContext.set(new UserContext("Thread 2 Info"));
             System.out.println("Thread 2: " + userContext.get().getUserInfo());
         }).start();
+
+        NonThreadLocalExample.main(new String[]{});
     }
 }
  class UserContext {
@@ -28,5 +30,33 @@ public class ThreadLocalExample {
 
     public void setUserInfo(String userInfo) {
         this.userInfo = userInfo;
+    }
+}
+class NonThreadLocalExample {
+    private static UserContext userContext = new UserContext("Initial Info");
+
+    public static void main(String[] args) {
+        // Запускаем два потока, которые работают с одним и тем же объектом UserContext.
+        new Thread(() -> {
+            userContext.setUserInfo("Thread 1 Info");
+            try {
+                Thread.sleep(100); // Задержка для наглядности
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("NonThreadLocalExample Thread 1: " + userContext.getUserInfo());
+            // Может вывести "Thread 1 Info" или "Thread 2 Info" в зависимости от тайминга
+        }).start();
+
+        new Thread(() -> {
+            userContext.setUserInfo("Thread 2 Info");
+            try {
+                Thread.sleep(100); // Задержка для наглядности
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("NonThreadLocalExample Thread 2: " + userContext.getUserInfo());
+            // Может вывести "Thread 1 Info" или "Thread 2 Info" в зависимости от тайминга
+        }).start();
     }
 }
